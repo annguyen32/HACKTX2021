523211ed890dc5844cdab7bb273187fdd7b3d037
Carwinism <carson10carly@yahoo.com>
2021-10-31 06:16:13 -0500

    Search for ingredients

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Search from './Search.js';
 import { useState } from 'react';
@@ -13,7 +12,7 @@ const filteredPosts = filterPosts(posts, searchQuery);
   return (<Router>
     <div className="App">
       <header className="App-header">
-        <h1> Welcome To Pantry Chef</h1>
+        <h1>Welcome To Pantry Chef</h1>
         <Search searchQuery={searchQuery}
                 setSearchQuery={setSearchQuery}/>
         <il>
@@ -27,10 +26,17 @@ const filteredPosts = filterPosts(posts, searchQuery);
  
 }
 const posts = [
-  { id: '1', name: 'Beef & Brocolli' },
-  { id: '2', name: 'Orange Chicken' },
-  { id: '3', name: 'Maccaronni & Cheese' },
-  { id: '4', name: 'Cheese Burger' },
+  { id: '1',  name: 'Beef & Brocolli',      ingredients: ["beef","soy sauce","brocolli","sugar","cornstarch","rice","oyster sauce"] },
+  { id: '2',  name: 'Orange Chicken',       ingredients: ["chicken breast","orange juice","cornstarch","ginger","garlic","soy sauce"] },
+  { id: '3',  name: 'Maccaronni & Cheese',  ingredients: ["macaroni","cheese"] },
+  { id: '4',  name: 'Cheese Burger',        ingredients: ["bread","bun","ground beef","cheese","ketchup","lettuce","tomato"] },
+  { id: '5',  name: 'Pancakes',             ingredients: ["flour","sugar","salt","baking soda","milk","butter"] },
+  { id: '6',  name: "Fried rice",           ingredients: ["rice","egg","carrot","onion","peas","soy sauce",] },
+  { id: '7',  name: "Karaage",              ingredients: ["chicken","flour","cornstarch","soy sauce"] },
+  { id: '8',  name: "Gumbo",                ingredients: ["flour","chicken broth","chicken","celery","onion","bell pepper","garlic"] },
+  { id: '9',  name: "Mapo Tofu",            ingredients: ["tofu","bean curd paste","ground pork","soy sauce"] },
+  { id: '10', name: "Egg Drop Soup",        ingredients: ["egg","chicken broth","ginger","green onion","cornstarch"] },
+  { id: '11', name: "Japanese Curry",       ingredients: ["beef","chicken","potato","carrot","onion","curry cube"] }
 ];
 
 
@@ -41,7 +47,8 @@ const filterPosts = (posts, query) => {
 
   return posts.filter((post) => {
       const postName = post.name.toLowerCase();
-      return postName.includes(query);
+      const postList = post.ingredients.filter((food) => food.includes(query)).length > 0;
+      return postName.includes(query) || postList;
   });
  
 };
--- my-app/src/Search.js
+++ my-app/src/Search.js
@@ -1,4 +1,4 @@
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 const SearchBar = ({searchQuery, setSearchQuery}) => (
     <form action="/" method="get">
